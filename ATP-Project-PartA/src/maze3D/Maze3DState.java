fd21cf549edadc1d233706726d5bd4b16574f0dc
eitanche <81564397+eitanche@users.noreply.github.com>
2021-04-02 18:25:51 +0300

    Position3D and Maze3dState created

--- ATP-Project-PartA/src/maze3D/Maze3DState.java
+++ ATP-Project-PartA/src/maze3D/Maze3DState.java
@@ -1,4 +1,44 @@
 package maze3D;
 
-public class Maze3DState {
+import algorithms.mazeGenerators.Position;
+import algorithms.search.AState;
+import algorithms.search.MazeState;
+
+public class Maze3DState extends AState {
+    private Position3D p;
+
+    public Maze3DState(Position3D p) {
+        this.p = p;
+        this.positionValue = 0;
+        this.father = null;
+        this.visited = false;
+    }
+
+    public Maze3DState(Position3D p,int positionValue) {
+        this.p = p;
+        this.positionValue = positionValue;
+        this.father = null;
+        this.visited = false;
+    }
+
+    public void setPositionValue(int val) {
+        positionValue = val;
+    }
+    public Position3D getPosition() {
+        return p;
+    }
+    public boolean equals(Object other) {
+        if (!(other instanceof Maze3DState))
+            return false;
+        return ((Maze3DState)other).getPosition().equals(p);
+    }
+
+    public String toString() {
+        return "{"+p.getDepthIndex()+","+(p.getRowIndex())+","+(p.getColumnIndex())+"}";
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
 }
--- ATP-Project-PartA/src/maze3D/Position3D.java
+++ ATP-Project-PartA/src/maze3D/Position3D.java
@@ -1,4 +1,34 @@
 package maze3D;
 
+import algorithms.mazeGenerators.Position;
+
 public class Position3D {
+    private int row;
+    private int column;
+    private int depth;
+
+    public Position3D(int depth,int row, int column) {
+        this.depth=depth;
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getDepthIndex() {return depth;}
+    public int getRowIndex() {
+        return row;
+    }
+    public int getColumnIndex() {
+        return column;
+    }
+
+    public boolean equals(Object other) {
+        if (!(other instanceof Position))
+            return false;
+        return ((Position3D)other).depth==this.depth && ((Position3D)other).row==this.row && ((Position3D)other).column==this.column;
+    }
+
+    public Position3D getBetween(Position3D p){
+        return new Position3D((p.depth-depth)/2+depth,(p.row-row)/2+row,(p.column-column)/2+column);
+    }
+
 }
