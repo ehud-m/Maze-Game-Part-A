467c96a5a9942253071225d8bddcceefd3e59419
eitanche <81564397+eitanche@users.noreply.github.com>
2021-05-11 15:21:17 +0300

    Added comments

--- ATP-Project-PartA/src/Server/SearchingAlogrithmFactory.java
+++ ATP-Project-PartA/src/Server/SearchingAlogrithmFactory.java
@@ -5,7 +5,15 @@ import algorithms.search.BreadthFirstSearch;
 import algorithms.search.DepthFirstSearch;
 import algorithms.search.ISearchingAlgorithm;
 
+/**
+ * This class is a factory which creates SearchingAlgorithm object.
+ * Used for the server according to the config file
+ */
 public class SearchingAlogrithmFactory {
+    /**
+     * Creates a searching algorithm object according to the property in the config file.
+     * @return
+     */
     public static ISearchingAlgorithm getSearchingAlgorithm(){
          switch (Configurations.getConfigInstance().loadProp().getProperty("mazeSearchingAlgorithm")){
                     case "BreathFirstSearch":
--- ATP-Project-PartA/src/Server/Server.java
+++ ATP-Project-PartA/src/Server/Server.java
@@ -1,10 +1,4 @@
 package Server;
-
-
-/*
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-*/
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,6 +6,9 @@ import java.net.SocketTimeoutException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * This class represents a multi-threaded server which receives requests from clients
+ */
 public class Server {
     private int port;
     private int listeningIntervalMS;
@@ -20,6 +17,12 @@ public class Server {
     private ExecutorService threadPool; // Thread pool
 
 
+    /**
+     * Constructs a Server object
+     * @param port The port of the server
+     * @param listeningIntervalMS The time to wait for a client
+     * @param strategy The strategy the server will use
+     */
     public Server(int port, int listeningIntervalMS, IServerStrategy strategy) {
         this.port = port;
         this.listeningIntervalMS = listeningIntervalMS;
@@ -27,17 +30,23 @@ public class Server {
         this.threadPool = Executors.newFixedThreadPool(Integer.parseInt(Configurations.getConfigInstance().loadProp().getProperty("threadPoolSize")));
     }
 
+    /**
+     * Starts the server run in a new thread
+     */
     public void start(){
 
         new Thread(() ->{
             runServer();
         }).start();
     }
+
+    /**
+     * The functionality of the server - runs and recieves clients until it's told to stop
+     */
     public void runServer(){
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningIntervalMS);
-
             while (!stop) {
                 try {
                     Socket clientSocket = serverSocket.accept();
@@ -55,6 +64,10 @@ public class Server {
         }
     }
 
+    /**
+     * Handles a client which connects to the server, using the strategy
+     * @param clientSocket The socket of the client which connected to the server
+     */
     private void handleClient(Socket clientSocket) {
         try {
             strategy.ServerStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
@@ -66,6 +79,9 @@ public class Server {
         }
     }
 
+    /**
+     * Stops the server run
+     */
     public void stop(){
    //     LOG.info("Stopping server...");
 
