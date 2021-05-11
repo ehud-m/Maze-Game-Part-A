2ec636e9f2a8da84a64b560b7f401fa6fdea8360
eitanche <81564397+eitanche@users.noreply.github.com>
2021-05-11 15:19:25 +0300

    Added comments to Client

--- ATP-Project-PartA/src/Client/IClientStrategy.java
+++ ATP-Project-PartA/src/Client/IClientStrategy.java
@@ -3,6 +3,9 @@ package Client;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * represents a strategy that the client will use
+ */
 public interface IClientStrategy {
     void clientStrategy(InputStream inFromServer, OutputStream outToServer);
 }
